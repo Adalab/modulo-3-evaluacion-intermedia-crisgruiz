07b193ddda87d0e552aa46504e6e9b643666d91e
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-02-24 09:00:37 +0100

    Clone repository

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import PokemonList from "./PokemonList";
+import data from "../data/data.json";
+
+class App extends React.Component {
+  render() {
+    return console.log("Hola");
+
+    // <PokemonList pokemon={data} />;
+  }
+}
+export default App;
---
+++ src/Components/PokemonList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// class PokemonList extends React.Component {
+//   render() {
+//     return console.log("Hola");
+//   }
+// }
+
+// export default PokemonList;
